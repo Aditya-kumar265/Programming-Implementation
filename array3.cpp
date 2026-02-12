200f0b175f9e29c354321f5ce8ffb4bce64620a1
Aditya kumar <ditya.adi.001@gmail.com>
2026-02-12 09:47:49 +0530

    Implement functions for array manipulations

--- array3.cpp
+++ array3.cpp
@@ -138,6 +138,173 @@ int main()
     }
     }
     */
+
+
+    // (Q 2) Give an array of 0s and 1s, find the largest consecutive sequence of  all 0s or 1s.
+    
+    
+    
+
+    
+    
+    
+    int arr[] = {0,0,0,0,1,1,1,0,1,1,0,1,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    int current_count = 0;
+    int max_count = 0;
+
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == 0) {
+            // If we find a zero, increment current streak
+            current_count++; 
+        } else {
+            //If we find a 1, check if the streak we just finished is the biggest
+            if(current_count > max_count) {
+                max_count = current_count;
+            }
+            //Reset current streak to 0
+            current_count = 0;
+        }
+    }
+
+    //Last : Check one last time after the loop ends
+    // (in case the array ends with the longest sequence of zeros)
+    if(current_count > max_count) {
+        max_count = current_count;
+    }
+
+    // Output results
+    cout << "The longest sequence of 0s is: " << max_count << endl; 
+    
+    
+    
+            //sir's method
+    
+    
+    int arr[] {0,0,1,1,1,1,0,0,0,0,0,0,0};
+    int maxLen = 0, count = 1;
+    int n = sizeof(arr)/sizeof(arr[0]);
+   
+    
+    for(int i = 1; i < n ; i++){
+        if(arr[i] == arr[i-1]){
+            count++;
+        }
+        else{
+            count = 1;
+        }
+        if(count > maxLen){
+            maxLen = count;
+        }
+    }
+    
+    cout << maxLen << endl;
+    
+    
+    
+    
+    // (Q 3)
+    
+    Given an array of integers, put all 1s to the end of the array, while 
+    maintaining the relative position of all the other elements.
+    
+    a[] = {3,1,5,6,1,1,2,1,4}
+    output[] = {3,5,6,2,4,1,1,1,1} 
+    
+    
+    int a[] = {3,1,5,6,1,1,2,1,4};
+    int size = sizeof(a)/sizeof(a[0]);
+    int o[size];
+    int j = 0;
+    int count = 0;
+    
+    for(int i = 0;i<size ; i++){
+        if(a[i] !=1){
+            o[j] = a[i];
+            j++;
+        }
+    }
+    
+    
+    for(int i = j;i<size;i++){
+        o[i] = 1;
+    }
+    
+    //printing
+    
+    for(int i = 0;i<size;i++){
+        cout << o[i] << " " ;
+    }
+    
+    cout << endl ;
+    
+    
+            //Sir's method
+            
+    int arr[] = {5,6,1,4,1,8,1,9};
+    int pos = 0;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    for(int i = 0;i<n;i++){
+        if(arr[i]!=1){
+            arr[pos] = arr[i];
+            pos++;
+        }
+    }
+    while(pos<n){
+        arr[pos] = 1;
+        pos++;
+    }
+    
+    //printing
+    
+    for(int i = 0; i<n;i++){
+        cout << arr[i]<< " ";
+    } 
+    
+    
+    
+    //(Q 4)
+    
+    /*
+    Given an array of integers , reverse the array in groups of k.
+    a = {3,5,1,4,7,8,5,8} , k =3
+    o = {1,5,3,8,7,4,8,5}
+    
+    */ 
+    int a[] = {3,5,1,4,7,8,5,6};
+    int k = 3;
+    int size = sizeof(a)/sizeof(a[0]);
+    int pos = 0;
+    bool flag = true;
+    
+    while(flag){
+        int temp = a[pos];
+        a[pos] = a[pos + k-1] ;
+        a[pos + k-1] = temp;
+        pos += k;
+        cout << pos << endl ;
+        
+        flag = false;
+        
+        if( (size-pos) > 3){
+            flag = true;
+            
+        }
+        
+        else if(0<(size-pos) && (size-pos)<4){ // last few;
+            k = size-pos;
+            flag = true;
+        }
+        
+    }
+    
+    for(int i = 0;i<size ; i++){
+        cout << a[i] << " " ;
+    }
+    
+    cout << endl;
     
     
     
